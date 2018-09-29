aa745d6229a0cd0c68149713fb0fde243c6b2872
Joseph Huizar <joehuizar75@gmail.com>
2018-09-29 15:35:48 -0700

    First commit (Test)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LabAssignment3';
+
+private firstName: string;
+private lastName: string;
+
+  constructor() {
+    this.firstName ="Joseph"
+    this.lastName = "Huizar"
+  }
+  showGreeting() {
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName + '.';
+  }
+  hideGreeting() {
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'r1c2';
+  }
+
 }
